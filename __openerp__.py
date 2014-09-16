19b27bf7927aa91dab22c122da67052b02fe13dd
JorChong <chong.vivas@gmail.com>
2014-09-16 11:05:39 -0430

    Agregando archivos para declarar modulo

---
+++ __openerp__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+
+{
+    'name':'Curso OpenERP',
+    'descripcion':'Prueba el Curso Archivo OPENERP',
+    'author':'Jorge Chong',
+    'version':'dia1',
+    'depends':['base', ],
+    'data':[],
+    'demo':[],
+}
